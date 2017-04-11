3cc0f64e1a498d00a053ac8565f7d92a3c464dad
josh <joshremote@junk.josh.com>
2017-04-11 18:58:53 +0000

    Nicer test pattern with RGB scan left to right

--- ticktest.sh
+++ ticktest.sh
@@ -1,30 +1,49 @@
 #!/bin/sh
 
-# Scan though all active DHCP leases and send a GREEN screen to each
-# Then wiat a second and send a red bradcast. Repeat
+
+wipe () {
+
+	for row in $(seq 0 58); do 
+
+		./udpopc "$1" "$2" 0 $row 0 25
+		# sleep 0.01
+
+	done
+
+   	return 0
+}
+
+#stop though some nice colors
 
 while true; do  
 
-	# ':' is the delimiter here, and there are three fields on each line in the file
-	# IFS set below is restricted to the context of `read`, it doesn't affect any other code
-	while IFS=' ' read -r when mac ip hostname; do
-	  # process the fields
-	  # if the line has less than three fields, the missing fields will be set to to an empty string
-	  # if the line has more than three fields, `field3` will get the all the values, including the third field, including the delimiter(s)
 
-	  echo "Green to  $ip..."
+	for color in "FF0000" "00FF00" "0000FF"; do
 
-	  ./udpopc $ip 008000 0 59 0 24
-	  sleep 0.02
+		# Scan though all active DHCP leases and send a GREEN screen to each
+		# Then wiat a second and send a red bradcast. Repeat
 
-	done < leases.lnk
+		# ':' is the delimiter here, and there are three fields on each line in the file
+		# IFS set below is restricted to the context of `read`, it doesn't affect any other code
+		while IFS=' ' read -r when mac ip hostname; do
+		  # process the fields
+		  # if the line has less than three fields, the missing fields will be set to to an empty string
+		  # if the line has more than three fields, `field3` will get the all the values, including the third field, including the delimiter(s)
 
-	sleep 2
+		  echo "Color $color to ip $ip..."
 
-	# braodscast blue to all
+	      wipe $ip $color
 
- 	./udpopc 192.168.174.255 000080 0 59 0 24
+		done < leases.lnk
 
-	sleep 1
+		sleep 2
+
+		# braodscast off to all
+
+ 		./udpopc 192.168.174.255 000000 0 59 0 25
+
+		sleep 1
+
+	done
 
 done
