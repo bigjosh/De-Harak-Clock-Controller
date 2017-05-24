06e55ac7004cabf6f0176412f376a9ae52c68c94
josh <joshremote@junk.josh.com>
2017-05-24 23:26:03 +0000

    added sinebow cycle

---
+++ sinebow.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+## Read the list of ranbow cycle colors from sinebowcycle.txt
+## and broadcast to all digits
+
+while true; do  
+
+
+		while IFS=' ' read -r color; do
+
+			# braodscast off to all
+	 		./udpopc 192.168.174.255 $color 0 59 0 25
+
+		
+			sleep 0.1
+	
+
+		done < sinebowcycle.txt
+
+
+	done
+
+done
