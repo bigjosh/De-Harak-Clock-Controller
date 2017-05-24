879d53ad450522a317b650c6a0979022ec823e68
bigjosh <joshatclock@junk.josh.com>
2017-05-24 21:58:52 +0000

    Added new broadcast test and updated ticktest

--- hbroadwipetest.sh
+++ hbroadwipetest.sh
@@ -1,15 +1,17 @@
+
 #!/bin/sh
 
 
 wipe () {
 
-	for col in $(seq 0 25); do 
+	for col in $(seq 0 59); do 
 
-		./udpopc "$1" "$2" 0 59 0 $col
-#		./udpopc "$1" "$2" 0 59 0 25
+		./udpopc "$1" "$2" $col $((col+2)) 0 26
 		 sleep 0.02
 
 	done
+
+	sleep 0.1
 
    	return 0
 }
@@ -19,32 +21,32 @@ wipe () {
 while true; do  
 
 
-	for color in "8F0000" "008F00" "00008F"; do
+	for color in "FF0000" "00FF00" "0000FF"; do
 
 		# Scan though all active DHCP leases and send a GREEN screen to each
 		# Then wiat a second and send a red bradcast. Repeat
 
 		# ':' is the delimiter here, and there are three fields on each line in the file
 		# IFS set below is restricted to the context of `read`, it doesn't affect any other code
-		while IFS=' ' read -r when mac ip hostname; do
+	#	while IFS=' ' read -r when mac ip hostname; do
 		  # process the fields
 		  # if the line has less than three fields, the missing fields will be set to to an empty string
 		  # if the line has more than three fields, `field3` will get the all the values, including the third field, including the delimiter(s)
 
-		  echo "Color $color to ip $ip..."
+	#	  echo "Color $color to ip $ip..."
 
-	      wipe $ip $color
+	      wipe 192.168.174.255 $color 000000
 	   #   wipe 192.168.174.255 $color
 
-		done < leases.lnk
+	#	done < leases.lnk
 
-		sleep 2
+	#	sleep 2
 
 		# braodscast off to all
 
- 		./udpopc 192.168.174.255 000000 0 59 0 25
+ 	#	./udpopc 192.168.174.255 000000 0 59 0 25
 
-		sleep 1
+	#	sleep 1
 
 	done
 
