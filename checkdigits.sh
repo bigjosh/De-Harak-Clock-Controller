7bb0d9acf32a21d48b84b31f77665afb610beaf1
josh <joshremote@junk.josh.com>
2017-09-15 13:17:31 -0400

    Made clockmode more robust to missing DNS entries Added checkdigits.sh to give a visual display

---
+++ checkdigits.sh
@@ -0,0 +1,60 @@
+# show status of all digits visually
+# using DNS and ping
+
+
+echo "H means host not found in DNS"
+echo  "P means fialed to ping"
+echo
+
+# top row...
+
+for h in {1..12}; do
+
+    if addr=$(getent hosts $(printf "h%02d" $h)); then 
+        
+        ip=$(echo $addr | awk '{ print $1 }')        
+        
+        if ping -c 2 $ip >/dev/null; then         
+            printf "  h%02d " $h
+        else
+            printf " Ph%02dP" $h
+        fi
+    else
+    
+        printf " Hh%02dH" $h
+        
+    fi 
+done
+
+printf "\n"
+
+
+# bottom rows...
+
+for r in {0..4}; do 
+
+    for c in {0..11}; do 
+
+        m=$(((r*12)+c+1))
+
+
+        if addr=$(getent hosts $(printf "m%02d" $m)); then 
+            
+            ip=$(echo $addr | awk '{ print $1 }')        
+            
+            if ping -c 2 $ip >/dev/null; then         
+                printf "  m%02d " $m
+            else
+                printf " Pm%02dP" $m
+            fi
+        else
+        
+            printf " Hm%02dH" $m
+            
+        fi 
+    done
+    
+    printf "\n"
+        
+done
+
