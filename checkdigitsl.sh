7a3399d0e57eb2f587364934465fe37aa774bdc0
josh <joshremote@junk.josh.com>
2018-01-12 15:31:42 -0500

    Added checkdigitsl to make log lines for digit health changed ticket to run red, green, blue rather than red,while, blue

---
+++ checkdigitsl.sh
@@ -0,0 +1,61 @@
+# show status of all digits in a line suitable for logging
+
+# line starts with time followed by one letter per digit in order
+# .=Good,P=ping ailed, H=Host lookup failed
+
+
+# timestamp
+
+##printf $(date "+%y%m%d %k%M ")
+printf $(date "+%k%M ")
+
+# top row...
+
+for h in {1..12}; do
+
+    if addr=$(getent hosts $(printf "h%02d" $h)); then 
+        
+        ip=$(echo $addr | awk '{ print $1 }')        
+        
+        if ping -c 1 $ip >/dev/null; then         
+            printf "."
+        else
+            printf "P"
+        fi
+    else
+    
+        printf "H"
+        
+    fi 
+done
+
+
+# bottom rows...
+
+for r in {0..4}; do 
+
+    for c in {0..11}; do 
+
+        m=$(((r*12)+c))
+
+
+        if addr=$(getent hosts $(printf "m%02d" $m)); then 
+            
+            ip=$(echo $addr | awk '{ print $1 }')        
+            
+            if ping -c 1 $ip >/dev/null; then         
+                printf "."
+            else
+                printf "P"
+            fi
+        else
+        
+            printf "H"
+            
+        fi 
+    done
+    
+        
+done
+
+printf "\n"
