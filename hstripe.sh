8f97a6be1210e72d07d91556fc6ff890ab81d56a
Josh Levine <github@junk.josh.com>
2018-09-14 13:32:34 -0400

    Create hstripe.sh

---
+++ hstripe.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+
+
+# Cycle though colors, sending light a horazontal stripe that scans across all digits.
+# good for finding upsidedown digits and dead rows. 
+
+
+
+
+#step though some nice colors
+
+
+
+
+
+
+
+while true; do  
+
+
+
+	for color in "8f0000" "008f00" "00008f"; do
+
+
+
+
+
+
+
+		for row in {0..24}; do
+
+
+
+
+
+                ./udpopc 192.168.174.255 $color 0 59 0 25 60 $row
+
+
+
+          sleep 0.1                           
+
+
+
+		done 
+
+
+
+
+
+
+
+	done
+
+
+
+
+
+
+
+done
